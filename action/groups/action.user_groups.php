6d5bff32031982105afff6740fd7e6268285f935
fengxin5571 <502251245@qq.com>
2017-04-08 17:31:29 +0800

    加注释

--- action/groups/action.user_groups.php
+++ action/groups/action.user_groups.php
@@ -7,7 +7,7 @@ if($do == "add_groups"){
     if(!empty($admin_id)&&!empty($groups_users)&&is_array($groups_users)){
         
         $admin_name = $db->select(0,1,"rv_user","name",array("id = $admin_id"),"id desc");
-        array_unshift($groups_users, array($admin_id,$admin_name[name]));//将自己加入到群组中
+        array_unshift($groups_users, array($admin_id,$admin_name[name]));//将自己加入到群组数组中
         $ug_id=$db->insert(0, 2, "rv_users_groups", array("ug_admin_id = $admin_id"));//如果插入成功，则返回群组id
         if($ug_id){
             $sql="INSERT INTO rv_group_to_users(gu_gid,gu_uid,gu_group_nick) VALUES";
